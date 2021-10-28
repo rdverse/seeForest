4b9040f92c82b9aa24e6d1b0f0cb012fccffac63
Devesh Reddy <rdverse@alchemy.local>
2021-10-27 20:39:40 -0500

    safe point

--- seeForest.js
+++ seeForest.js
@@ -32,16 +32,16 @@ function diagonal(s, d,i) {
 //console.log(ruleData[i]);
 
 //console.log("parent: " + d["data"]["name"] + "   child :" +  s["data"]["name"] )
-for(rule in ruleData[i]){
 
+for(rule in ruleData[i]){
 }
 var xmul;
 	try{
-		console.log("try");
+		// console.log("try");
 		xmul = squareDim*s.data["val"] + squareDim*(s.data["feat"]);
 	}
 	catch{
-		console.log("catch");
+		// console.log("catch");
 		xmul = squareDim;
 	}
 
@@ -62,6 +62,7 @@ if(Number.isNaN(xmul)){
 	
 	var childName = s.data.name;
 	var parentName = d.data.name;
+	var childNameCopy = childName;
 
 	if(childName=="leaf"){
 	childName = parentName;	
@@ -70,20 +71,37 @@ if(Number.isNaN(xmul)){
 	var xmul;
 
 //	console.log(jsonData[childName][ruleIndex]);
+	positionC = positions[childNameCopy];
+	positionP = positions[parentName];
 
-	xmul = squareDim* dataSlice[childName] + squareDim*2;
+	// console.log("postiions");
+	// console.log(positionP);
+	// console.log(positionC);
 
-	console.log(xmul);
+	xmulP = squareDim* dataSlice[childName] + squareDim*positionP;
+	xmulC = squareDim* dataSlice[childName] + squareDim*positionC;
+
+	// console.log("xmuls");
+	// console.log(xmulP);
+	// console.log(xmulC);
+
+	// console.log(xmul);
 	
 	if(Number.isNaN(xmul)){
 		xmul = squareDim+ squareDim*1;
 	}
+
+
+		path = `M ${s.y}, ${s.x+xmulP}
+				C ${(s.y + d.y) / 2} ${s.x+xmulP},
+				${(s.y + d.y) / 2} ${d.x+ xmulC},
+				${d.y+squareDim} , ${d.x+xmulC}`
 	
-		path = `M ${s.y}, ${s.x+xmul}
-				C ${(s.y + d.y) / 2} ${s.x+xmul},
-				${(s.y + d.y) / 2} ${d.x+xmul},
-				${d.y+squareDim} , ${d.x+xmul}`
-	
+					// return "M" + d.source.y + "," + d.source.x
+					// 	+ "C" + (d.source.y + d.target.y) / 2 + "," + d.source.x
+					// 	+ " " + (d.source.y + d.target.y) / 2 + "," + d.target.x
+					// 	+ " " + d.target.y + "," + d.target.x;
+
 		return path
 	  }
 	
@@ -111,8 +129,8 @@ var root = d3.hierarchy(jsonData, function(d){return d.children;});
 // Set the start point of root node
 root.x0 = height/2;
 root.y0 = 0;
-console.log("root");
-console.log(root);
+// console.log("root");
+// console.log(root);
 
 //get x and y coordinates for nodes
 var treeData = treeMap(root);
@@ -124,6 +142,11 @@ links = treeData.descendants().slice(1);
 
 //normalize depth
 nodes.forEach(d => {d.x = d.x - 100;d.y = d.depth*150;});
+
+// nodes.forEach(d => {d.x = d.x + squareDim*(positions[d.data.name]); console.log(positions[d.data.name]);});
+
+nodes.forEach(d => {d.x = d.x + squareDim*(positions[d.data.name]);});
+
 
 return [root, nodes, links];
 }
@@ -149,6 +172,7 @@ return nodeEnter;
 }
 
 function draw_nodes(root,treeIndex, nodes){
+
 	var i = 0;
 	var node = canvas.selectAll(".node" + String(treeIndex))
 				.data(nodes, function(d){return d.id ||(d.id = ++i);});
@@ -164,10 +188,8 @@ function draw_nodes(root,treeIndex, nodes){
 			.attr('class', 'node')
 			.attr('width', squareDim)
 			.attr('height', squareDim)
-			.attr('opacity', 0.8)
-			.attr("fill", "blue")
 			.on("pointerover", function(p,d){
-						//console.log(d);
+				//console.log(d);
 				});
 
 	nodeEnter = add_text(nodeEnter);
@@ -177,7 +199,9 @@ function draw_nodes(root,treeIndex, nodes){
 	nodeUpdate.transition()
 	.duration(750)
 	.attr("transform", function(d) { 
-		return "translate(" + d.y + "," + (d.x+d.data.feat*squareDim) + ")";
+		
+		return "translate(" + d.y + "," + (d.x + (squareDim*(positions[d.data.name])))+ ")";
+
 	});
 
 	nodeUpdate.select('rect.node')
@@ -186,18 +210,14 @@ function draw_nodes(root,treeIndex, nodes){
 	.attr('opacity', 0.2)
 	.attr("fill", "blue")
 	.style("stroke", "black");
-
 }
 
 function draw_links(root,treeIndex, links){
-
-	
 	// console.log("links")
 	// console.log(links["0"]);
 
 	// console.log("data slice");
 	// console.log((jsonData)=> {return jsonData});
-	
 
 	colors = {0:"red", 1: "blue", 2:"green"}
 
@@ -209,7 +229,7 @@ function draw_links(root,treeIndex, links){
 			dataSlice[key] = jsonData[key][i];
 		};
 
-		console.log(i);
+		// console.log(i);
 		var link = canvas.selectAll('path.link' + String(treeIndex) + "_" + String(i))
 					.data(links, function(d){return d.id;});
 		
@@ -223,7 +243,7 @@ function draw_links(root,treeIndex, links){
 
 		var linkUpdate = linkEnter.merge(link);
 
-		linkUpdate.attr('d', function(d,rule){ return (console.log(ruleData[rule][i], rule))});
+		// linkUpdate.attr('d', function(d,rule){ return (console.log(ruleData[rule][i], rule))});
 
 		// linkUpdate.attr('d', function(d,rule){ return (ruleData[rule][i]==1)?console.log("some"):console.log("none");});
 		linkUpdate.attr('d', function(d,rule){ return (ruleData[String(rule)][i]==1)?diagonals(d, d.parent,dataSlice):null;});
